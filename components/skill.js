3967f46d1bfc3230b3a9e0b77595819521835c05
Rakesh kumar <lakshmankumarr121@gmail.com>
2023-10-14 21:46:21 +0530

    he

--- components/skill.js
+++ components/skill.js
@@ -1,8 +1,12 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
 import styles from '../styles/Skill.module.css';
-
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
 
 const Skill = () => {
-    let lan = [{'lang' : 'Javascript', 
+    let lan = [
+        {'lang' : 'Javascript', 
         'image' : 'abc.png',
         'experience' : 'Junior',
         id : 0
@@ -54,27 +58,68 @@ const Skill = () => {
                                 'experience' : 'Junior',
                                     id : 8
                                 } 
+        // Add more skill objects here
     ];
 
- 
-    
-    const items = lan?.map((details4) => {
-        return (
-          <div className="mylang" title={details4.lang_name} key={details4.id}>
-            {details4.id}
-            <br />
-            <br />
-          </div>
-        );
-      });
-    return (
-      <div className={styles.skbg}>
-        <div className={styles.myskil}>MY SKILLS PROGRESS SO FAR</div>
-        <div className={styles.mysl}>My Skills</div>
-  
+    const skillItems = lan.map((skill) => (
+        <div key={skill.id}>
+            <div>{skill.id}</div>
+            
+            {/* Add any additional information you want to display for each skill */}
+        </div>
+    ));
+    const [isClient, setIsClient] = useState(false);
+
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
+    if (!isClient) {
         
-      </div>
+        return null;
+    }
+    var responsive = {
+        0: {
+          items: 1,
+        },
+        380: {
+          items: 1,
+        },
+        512: {
+          items: 2,
+        },
+        665: {
+          items: 3,
+        },
+        767: {
+          items: 3,
+        },
+        870: {
+          items: 4,
+        },
+        1024: {
+          items: 6,
+        },
+        1265: {
+          items: 4,
+        },
+      };
+
+    return (
+        <div className={styles.skbg}>
+            <div className={styles.myskil}>MY SKILLS PROGRESS SO FAR</div>
+            <div className={styles.mysl}>My Skills</div>
+            <AliceCarousel
+                infinite
+                disableButtonsControls
+                autoPlay
+                mouseTracking
+                autoPlayInterval={1000}
+                responsive={responsive}
+                items={skillItems}
+            />
+        </div>
     );
-  };
-  
-  export default Skill;
+};
+
+export default Skill;
